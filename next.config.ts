b42fd1cb23a6ef367f88aa5657d918986b3b4047
KrongDev <leegeondev@gmail.com>
2025-03-27 04:14:34 +0900

    fix: Docker

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+const path = require('path');
 
 const nextConfig: NextConfig = {
   /* config options here */
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname, 'src'),
+        };
+        return config;
+    },
     async redirects() {
         return [
             {
