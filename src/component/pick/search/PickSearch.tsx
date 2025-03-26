1c226fd40606ea9408514d8cc584abc9ee76c2df
Leegeon <leegeondev@gmail.com>
2025-03-27 04:41:14 +0900

    feat: op.gg 검색 이동 구현

--- src/component/pick/search/PickSearch.tsx
+++ src/component/pick/search/PickSearch.tsx
@@ -1,18 +1,43 @@
+import {ChangeEvent, useState} from "react";
 
+const opGGUrl = 'https://www.op.gg/summoners/'
 
 export const PickSearch = () => {
+
+    const [ searchText, setSearchText ] = useState('');
+
+    const onChangeSearchText = (e: ChangeEvent<HTMLInputElement>) => {
+        setSearchText(e.target.value)
+    }
+
+    const enterSearch = (keyCord: number) => {
+
+        if(keyCord !== 13) return;
+
+        if(searchText === '') return alert('검색어를 입력해주세요');
+
+        if(!searchText.includes('#')) {
+            window.location.href = `${opGGUrl}/search?q=${searchText}&region=kr`
+            return;
+        }
+        const searchData = searchText.replace(' #', '-')
+        window.location.href = `${opGGUrl}/kr/${searchData}`
+    }
+
     return (
         <div className="z-10 flex flex-col items-center pt-10">
             <h1 className="text-white text-4xl font-bold">.GG</h1>
             <div className="mt-4 flex items-center bg-white px-4 py-2 rounded-full shadow-lg">
                 <select className="border-none outline-none">
-                    <option>Korea</option>
-                    <option>NA</option>
+                    <option defaultChecked >Korea</option>
                 </select>
                 <input
                     type="text"
                     placeholder="플레이어 이름 + #KR1"
                     className="ml-4 px-2 py-1 outline-none"
+                    value={searchText}
+                    onChange={onChangeSearchText}
+                    onKeyUp={e => enterSearch(e.keyCode)}
                 />
             </div>
         </div>
