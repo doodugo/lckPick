d00ba5b1d7da6659a4eb9afaf26d826b3024dd20
Leegeon <leegeondev@gmail.com>
2025-03-27 04:46:09 +0900

    feat: Header 피드백 버튼 추가

--- src/component/layout/HeaderLayout.tsx
+++ src/component/layout/HeaderLayout.tsx
@@ -1,8 +1,17 @@
+'use client'
 
 export const HeaderLayout = () => {
+    const onClickPidBack = () => {
+
+    }
     return (
-        <header className="fixed top-0 left-0 w-full h-8 bg-gradient-to-r from-blue-800 via-purple-700 to-indigo-700 text-white flex items-center px-4 z-10">
-            LoL Pick
+        <header className="fixed justify-between top-0 left-0 w-full h-8 bg-gradient-to-r from-blue-800 via-purple-700 to-indigo-700 text-white flex items-center px-4 z-10">
+            <span>
+                LoL Pick
+            </span>
+            <div className={'cursor-pointer'} onClick={onClickPidBack}>
+                피드백
+            </div>
         </header>
 
     );
